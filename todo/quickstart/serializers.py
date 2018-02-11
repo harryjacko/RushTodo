15d33865217d7963b23713375a23f2de31cc38a9
Harry Jackson <hjac660@aucklanduni.ac.nz>
2018-02-12 11:15:27 +1300

    Created Serializers

---
+++ todo/quickstart/serializers.py
@@ -0,0 +1,13 @@
+from quickstart.models import User, TodoItem
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = User
+		fields = ('id', 'user_name', 'password', 'created', 'google_auth')
+
+class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
+	user = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+	class Meta:
+		model = TodoItem
+		fields = ('id', 'name', 'remind_date', 'repeat_remind', 'remind_location', 'image')
