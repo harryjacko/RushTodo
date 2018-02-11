88625051a1c985aa69548ddebdde94b48536da47
Harry Jackson <hjac660@aucklanduni.ac.nz>
2018-02-12 11:36:37 +1300

    Basic Views setup

--- todo/quickstart/views.py
+++ todo/quickstart/views.py
@@ -1,3 +1,33 @@
+from quickstart.models import User, TodoItem
+from quickstart.serializers import UserSerializer, TodoItemSerializer
 from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+
+# ------- User -------
+class UserList(generics.ListCreateAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+	lookup_field = ('user_name')
+
+class UserFromIdDetial(generics.RetrieveUpdateDestroyAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+	lookup_field = ('id')
+
+# ------- TodoItem -------
+class TodoItemFromUserList(generics.ListCreateAPIView):
+	queryset = TodoItem.objects.all()
+	serializer_class = TodoItemSerializer
+
+	def filter_queryset(self, request):
+		user_form = self.kwargs['user']
+		get_object_or_404(User, id=user_form)
+		queryset = TodoItem.objects.filter(user=user_form)
+		serializer = TodoItemSerializer(queryset, many=True)
+		return queryset
