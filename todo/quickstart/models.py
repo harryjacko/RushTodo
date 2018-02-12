476abf4abfc918e0ccc4d6723b92b27cc673cc10
Harry Jackson <hjac660@aucklanduni.ac.nz>
2018-02-12 13:06:53 +1300

    Update to models

--- todo/quickstart/models.py
+++ todo/quickstart/models.py
@@ -2,7 +2,7 @@ from django.db import models, IntegrityError
 
 # Create your models here.
 class User(models.Model):
-	user_name = models.CharField(max_length=20, unique=True)
+	user_name = models.CharField(max_length=50, unique=True)
 	password = models.CharField(max_length=256)
 	created = models.DateTimeField(auto_now_add=True)
 	google_auth = models.BooleanField(default=False)
@@ -10,6 +10,7 @@ class User(models.Model):
 class TodoItem(models.Model):
 	name = models.CharField(max_length=50)
 	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	completed = models.BooleanField(default=False)
 	remind_date = models.DateTimeField(blank=True, null=True)
 	repeat_remind = models.BooleanField(default=False, blank=True)
 	remind_location = models.CharField(max_length=300, blank=True)
