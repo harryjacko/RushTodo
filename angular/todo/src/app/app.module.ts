e765298b73d1981decc92019c2070e249035cfcd
Harry Jackson <hjac660@aucklanduni.ac.nz>
2018-02-12 17:03:11 +1300

    Building login form

--- angular/todo/src/app/app.module.ts
+++ angular/todo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -7,19 +8,24 @@ import { IndexComponent } from './index/index.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatInputModule } from '@angular/material';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		IndexComponent
+		IndexComponent,
+		RegisterComponent
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
+		FormsModule,
+		ReactiveFormsModule,
 		BrowserAnimationsModule,
 		MatButtonModule,
 		MatToolbarModule,
+		MatInputModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
--- angular/todo/src/app/index/index.component.ts
+++ angular/todo/src/app/index/index.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+	selector: 'app-index',
+	templateUrl: './index.component.html',
+	styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+	emailFormControl = new FormControl('', [
+		Validators.email,
+	]);
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	loginUser(user_email : String, user_password : String){
+		console.log(user_email + " + " + user_password);
+	}
 
 }
