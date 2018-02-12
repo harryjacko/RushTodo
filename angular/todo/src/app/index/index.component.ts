f4e864fde9d47d2cc171218e74ee987e9bad453b
Harry Jackson <hjac660@aucklanduni.ac.nz>
2018-02-12 17:34:05 +1300

    Updated to allow cors

--- angular/todo/src/app/index/index.component.ts
+++ angular/todo/src/app/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -11,14 +12,16 @@ export class IndexComponent implements OnInit {
 	emailFormControl = new FormControl('', [
 		Validators.email,
 	]);
-
-	constructor() { }
+	private baseUrl: string = "http://127.0.0.1:8000/"
+	constructor(private http: HttpClient) { }
 
 	ngOnInit() {
 	}
 
 	loginUser(user_email : String, user_password : String){
-		console.log(user_email + " + " + user_password);
+		this.http.get(this.baseUrl + "users/").subscribe(data => {
+			console.log(data);
+		});
 	}
 
 }
