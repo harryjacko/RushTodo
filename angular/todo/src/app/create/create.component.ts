0f23110821546738d9dab108397a910311c2a65f
Harry Jackson <hjac660@aucklanduni.ac.nz>
2018-02-13 14:33:29 +1300

    Gets user input

--- angular/todo/src/app/create/create.component.ts
+++ angular/todo/src/app/create/create.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import { HttpClient } from '@angular/common/http';
+import { User } from "../user";
+import { Todo } from "../todo";
+import { JsonResponse } from "../json.response";
+import { SessionService } from "../session.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+	selector: 'app-create',
+	templateUrl: './create.component.html',
+	styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+	constructor(private http: HttpClient, private sessionService: SessionService, private router: Router) { }
+	user = null;
+	errorMessage = "";
+	private baseUrl: string = "http://127.0.0.1:8000/";
 
-  ngOnInit() {
-  }
+	// todoDetails = {
+	// 	name: '',
+	// 	user: null,
+	// 	completed: false,
+	// 	remind_date: null,
+	// 	repeat_remind: false,
+	// 	remind_image: '',
+	// 	image: ''
+	// }
+
+	ngOnInit() {
+		this.user = this.sessionService.getUser();
+		if (this.user == null){
+			this.router.navigate(['/index']);
+		}
+	}
+
+	submitTodo(name, date){
+		// HAVE A FUNCTION THAT CHECKS ITS THE RIGHT FORMAT?
+		if (date ==''){
+			date = null;
+		}
+		let todo = <Todo>({
+			id: null,
+			name: name,
+			user: this.user,
+			completed: false,
+			remind_date: date,
+			repeat_remind: false,
+			remind_location: '',
+			image: ''
+		});
+
+	}
 
 }
