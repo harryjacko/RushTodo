50ad7442a6a9152cb2dae8feccbc50239410cda4
Harry Jackson <hjac660@aucklanduni.ac.nz>
2018-02-14 13:39:41 +1300

    Settings update to work with mysql db

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		# 'ENGINE': 'django.db.backends.sqlite3',
+		# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'rush',
+		'USER': 'root',
+		'PASSWORD': 'rush2018',
 	}
 }
 
